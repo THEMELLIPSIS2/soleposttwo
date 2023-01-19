ef18277b1809a259b9ba0cbb505b1687bb06f278
Eve Frances <evefrances92@icloud.com>
2023-01-19 15:00:00 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NavBar from './components/Nav'
+import {useSelector} from 'react-redux'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>I Love Shoes</p>
