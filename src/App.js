9cb70d07ee765c17716a51ea0e68b2f56143d899
Noelle Huchette <noelle.huchette@gmail.com>
2023-01-19 15:26:05 -0500

    configuring supabase auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
+import supabase from './supabase';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  supabase.auth.onAuthStateChange((e, session) => {
+    if (session?.user) {
+      setUser((user) => (user = session.user));
+    }
+  });
+
+  async function signInWithGithub() {
+    const { user, session, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github'
+    });
+  }
+
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    setUser((user) => (user = null));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>I Love Shoes</p>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>I Love Shoes</p>
+      {!user ? (
+        <button onClick={signInWithGithub}>SIGN IN WITH GITHUB</button>
+      ) : (
+        <button onClick={signOut}>SIGN OUT {user?.email}</button>
+      )}
     </div>
   );
 }
