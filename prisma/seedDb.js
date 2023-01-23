92adb4b971379313fa5487c3309f88c602945e3e
Noelle Huchette <noelle.huchette@gmail.com>
2023-01-23 14:45:12 -0500

    simple seed data

--- prisma/seedDb.js
+++ prisma/seedDb.js
@@ -1,17 +1,57 @@
 const prisma = require('./prismaClient.js');
-const faker = require('faker');
+const { faker } = require('@faker-js/faker');
+
+const tags = [
+  'Nike',
+  'Adidas',
+  'Puma',
+  'Converse',
+  'Reebok',
+  'New-Balance',
+  'Vans',
+  'Jordan',
+  'Yeezy',
+  'Asics',
+  'Basketball',
+  'Sneakers',
+  'HipHop',
+  'Misc',
+  'Funny',
+  'Event',
+  'Release',
+  'Rumor'
+];
 
 const users = [];
+const articles = [];
 
-function createUsers() {
-  const num = 100;
+function pickTags(num = 2) {
+  let assigned = [];
   let x = 0;
 
-  while (x < 100) {
+  while (x < num) {
+    let tag = tags[Math.floor(Math.random() * tags.length)];
+    if (assigned.includes(tag)) continue;
+    assigned.push(tag);
+    x++;
+  }
+  return assigned;
+}
+
+function createData(num) {
+  let x = 0;
+
+  while (x < num) {
     const user = {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email()
+      email: faker.internet.email(),
+      name: faker.name.fullName(),
+      articles: {
+        create: {
+          title: faker.lorem.words(5),
+          content: faker.lorem.paragraphs(5, '\n'),
+          published: true
+        }
+      }
     };
     users.push(user);
     x++;
@@ -19,12 +59,29 @@ function createUsers() {
 }
 
 async function seedDb() {
-  await prisma.user.createMany({ data: users });
+  for (let user of users) {
+    await prisma.user.create({
+      data: user,
+      include: {
+        articles: true
+      }
+    });
+  }
 }
 
 async function main() {
-  createUsers();
+  createData(0);
   await seedDb();
+
+  // const genTags = tags.map((tag) => {
+  //   return {
+  //     name: tag
+  //   };
+  // });
+  // await prisma.tags.createMany({
+  //   data: genTags,
+  //   skipDuplicates: true
+  // });
 }
 
 main()
